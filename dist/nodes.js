cb06ed18f53e9f45517279e3392e7e0e2a1e4cc4
kratark <alexander_p_r@ymail.com>
2016-03-24 00:59:48 -0400

    feature
    supporting feature to render categories

---
+++ dist/nodes.js
@@ -0,0 +1,76 @@
+USETEXTLINKS = 1
+STARTALLOPEN = 0
+USEFRAMES = 0
+USEICONS = 0
+WRAPTEXT = 1
+PRESERVESTATE = 1
+
+
+foldersTree = gFld("<b>Ebay</b>","267.html")
+foldersTree.treeID = "Ebay"
+_11491187 = insFld(foldersTree, gFld("Books", "javascript:undefined"))
+_1660460877 = insFld(_11491187, gFld("Children & Young Adults", "javascript:undefined"))
+_1592547779 = insFld(_11491187, gFld("Cookbooks", "javascript:undefined"))
+_2043882716 = insFld(_11491187, gFld("Catalogs", "javascript:undefined"))
+_1601985924 = insFld(_2043882716, gFld("Agriculture", "javascript:undefined"))
+_808471126 = insFld(_2043882716, gFld("Art & Exhibitions", "javascript:undefined"))
+_1581247944 = insFld(_2043882716, gFld("Auction", "javascript:undefined"))
+_1158421740 = insFld(_2043882716, gFld("Clothing & Fashion", "javascript:undefined"))
+_68703119 = insFld(_2043882716, gFld("Department Stores", "javascript:undefined"))
+_1728865894 = insFld(_2043882716, gFld("Music & Instruments", "javascript:undefined"))
+_394299439 = insFld(_2043882716, gFld("Owner's Manuals", "javascript:undefined"))
+_40841685 = insFld(_2043882716, gFld("Toys & Hobbies", "javascript:undefined"))
+_69464528 = insFld(_2043882716, gFld("Other Catalogs", "javascript:undefined"))
+_401015153 = insFld(_11491187, gFld("Fiction & Literature", "javascript:undefined"))
+_2142930933 = insFld(_11491187, gFld("Nonfiction", "javascript:undefined"))
+_711879937 = insFld(_11491187, gFld("Textbooks, Education", "javascript:undefined"))
+_1598609485 = insFld(_11491187, gFld("Other Books", "javascript:undefined"))
+_511322725 = insFld(_11491187, gFld("Magazine Back Issues", "javascript:undefined"))
+_389709944 = insFld(_11491187, gFld("Antiquarian & Collectible", "javascript:undefined"))
+_1055096997 = insFld(_11491187, gFld("Wholesale & Bulk Lots", "javascript:undefined"))
+_1967771591 = insFld(_1055096997, gFld("Other Wholesale Books", "javascript:undefined"))
+_1783697654 = insFld(_11491187, gFld("Audiobooks", "javascript:undefined"))
+_774410805 = insFld(_11491187, gFld("11-50 Items", "javascript:undefined"))
+_660078259 = insFld(_11491187, gFld("101-500", "javascript:undefined"))
+_334903266 = insFld(_11491187, gFld("Up to 5 Items", "javascript:undefined"))
+_1515592108 = insFld(_11491187, gFld("More than 500", "javascript:undefined"))
+_1077359410 = insFld(_11491187, gFld("6-10 Items", "javascript:undefined"))
+_677270508 = insFld(_11491187, gFld("51-100 Items", "javascript:undefined"))
+_1470853108 = insFld(_1055096997, gFld("Magazines", "javascript:undefined"))
+_1558919789 = insFld(_11491187, gFld("Accessories", "javascript:undefined"))
+_674248303 = insFld(_1558919789, gFld("Book Lights", "javascript:undefined"))
+_1917369809 = insFld(_1558919789, gFld("Bookends", "javascript:undefined"))
+_412809887 = insFld(_1558919789, gFld("Book Stands, Holders", "javascript:undefined"))
+_1935913451 = insFld(_1558919789, gFld("Address Books", "javascript:undefined"))
+_1930044475 = insFld(_1558919789, gFld("Blank Diaries & Journals", "javascript:undefined"))
+_825404974 = insFld(_1558919789, gFld("Book Covers", "javascript:undefined"))
+_1707918074 = insFld(_1558919789, gFld("Bookmarks", "javascript:undefined"))
+_1019886723 = insFld(_1558919789, gFld("Other Book Accessories", "javascript:undefined"))
+_1569934225 = insFld(_1558919789, gFld("Book Plates", "javascript:undefined"))
+
+
+
+
+
+
+/*
+
+foldersTree = gFld("<b>Tree Options</b>", "demoFrameless.html")
+  foldersTree.treeID = "Frameless"
+  
+  
+  aux1 = insFld(foldersTree, gFld("Expand for example with pics and flags", "javascript:undefined"))
+    aux2 = insFld(aux1, gFld("United States", "demoFrameless.html?pic=%22beenthere_unitedstates%2Egif%22"))
+      insDoc(aux2, gLnk("S", "Boston", "demoFrameless.html?pic=%22beenthere_boston%2Ejpg%22"))
+      insDoc(aux2, gLnk("S", "Tiny pic of New York City", "demoFrameless.html?pic=%22beenthere_newyork%2Ejpg%22"))
+      insDoc(aux2, gLnk("S", "Washington", "demoFrameless.html?pic=%22beenthere_washington%2Ejpg%22"))
+    aux2 = insFld(aux1, gFld("Europe", "demoFrameless.html?pic=%22beenthere_europe%2Egif%22"))
+      insDoc(aux2, gLnk("S", "London", "demoFrameless.html?pic=%22beenthere_london%2Ejpg%22"))
+      insDoc(aux2, gLnk("S", "Lisbon", "demoFrameless.html?pic=%22beenthere_lisbon%2Ejpg%22"))
+  aux1 = insFld(foldersTree, gFld("Types of node", "javascript:undefined"))
+    aux2 = insFld(aux1, gFld("Expandable with link", "demoFrameless.html?pic=%22beenthere_europe%2Egif%22"))
+      insDoc(aux2, gLnk("S", "London", "demoFrameless.html?pic=%22beenthere_london%2Ejpg%22"))
+    aux2 = insFld(aux1, gFld("Expandable without link", "javascript:undefined"))
+      insDoc(aux2, gLnk("S", "NYC", "demoFrameless.html?pic=%22beenthere_newyork%2Ejpg%22"))
+    insDoc(aux1, gLnk("B", "Opens in new window", "http://www.treeview.net/treemenu/demopics/beenthere_pisa.jpg"))
+*/
